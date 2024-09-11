f76b7f3121d5b255b4734b67625cb3bebaa8cf10
peters <petes726@gmail.com>
2024-09-11 13:04:15 -0400

    Completing day 2 part 1

--- Day2.py
+++ Day2.py
@@ -26,3 +26,36 @@ In the example above, games 1, 2, and 5 would have been possible if the bag had 
 
 Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
 """
+import re
+
+gameValues = []
+viableGameSum = 0
+viableGameCount = 0
+
+with open("Day2_Input.txt") as file:
+    for line in file:
+
+        idMatches = re.search("(?<=Game )\d+(?=:)", line)
+        gameId = idMatches.group(0)
+
+        redMatches = re.findall("(?<=\s)\d+(?= red)", line)
+        redInts = [int(i) for i in redMatches]
+        maxReds = int(max(redInts))
+
+        greenMatches = re.findall("(?<=\s)\d+(?= green)", line)
+        greenInts = [int(i) for i in greenMatches]
+        maxGreens = int(max(greenInts))
+
+
+        blueMatches = re.findall("(?<=\s)\d+(?= blue)", line)
+        blueInts = [int(i) for i in blueMatches]
+        maxBlues = int(max(blueInts))
+
+        gameValues.append({"id": gameId, "greens": maxGreens, "reds": maxReds, "blues": maxBlues})
+
+for game in gameValues:
+    if ((game["reds"] <= 12) and (game["greens"] <= 13) and (game["blues"] <= 14)):
+        viableGameSum += int(game["id"])
+        viableGameCount += 1
+
+print(viableGameSum)
