c5c2e5a01286d819a1ba8d2877f27a7de2d72d0b
peters <petes726@gmail.com>
2024-09-10 21:25:03 -0400

    Completed day 1 part 2

--- Day1.py
+++ Day1.py
@@ -23,14 +23,56 @@ In this example, the calibration values of these four lines are 12, 38, 15, and 
 
 Consider your entire calibration document. What is the sum of all of the calibration values?
 """
+
+# import re
+
+# calibrationTotal = 0
+
+# with open("Day1_Input.txt") as file:
+#     for line in file:
+#         lineInts = re.sub("[^\d\.]", "", line)
+#         calibrationStr = lineInts[0] + lineInts[-1]
+#         calibrationInt = int(calibrationStr)
+#         calibrationTotal += calibrationInt
+
+# print(calibrationTotal)
+
+"""--- Part Two ---
+
+Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+What is the sum of all of the calibration values?
+"""
+
 import re
 
 calibrationTotal = 0
 
+def replace_strings(string, dict):
+    for i, j in dict.items():
+        string = string.replace(i, j)
+    return string
+
+replacements = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
 with open("Day1_Input.txt") as file:
     for line in file:
-        lineInts = re.sub("[^\d\.]", "", line)
-        calibrationStr = lineInts[0] + lineInts[-1]
+        matches = re.finditer(r"(?=([1-9]|one|two|three|four|five|six|seven|eight|nine))", line)
+        matchGroup = [match.group(1) for match in matches]
+        matchInts =  [replace_strings(i, replacements) for i in matchGroup]
+        calibrationStr = matchInts[0] + matchInts[(len(matchInts) - 1)]
         calibrationInt = int(calibrationStr)
         calibrationTotal += calibrationInt
 
